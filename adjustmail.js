02efeb5b457bd43a78a1624e50a3cc1dd2667cab
JakobScholbach <58555205+JakobScholbach@users.noreply.github.com>
2022-07-01 21:41:19 +0200

    Add files via upload

--- adjustmail.js
+++ adjustmail.js
@@ -2,7 +2,7 @@ function adjust(s)
 {
   s3 = '@';
   s4 = 'uni';
-  s5 = '-muenster.de';
+  s5 = 'pd.it';
   document.getElementById('eMailLink').href = 'mailto:'+s+s3+s4+s5;
   document.getElementById('eMailLink').innerHTML = s+s3+s4+s5;
 }
